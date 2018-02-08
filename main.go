bdf6fb60930dd0c0883eab94440bf1d326c196df
Victor Unegbu <victor@unegbu.com>
2018-02-08 02:57:34 -0600

    tweak HTTP API, simplify SSM service calls

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-	"log"
-	"github.com/gorilla/mux"
-	"github.com/gorilla/handlers"
-	"os"
-	"net/http"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"crypto/sha256"
-	"strings"
 	"io"
+	"log"
+	"net/http"
+	"os"
 	"strconv"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+const DEFAULT_ENVIRONMENT = "dev"
+
+var (
+	currentEnvironment = DEFAULT_ENVIRONMENT
+	DebugMode          = false
+	region             = os.Getenv("AWS_REGION")
+	debug              = os.Getenv("DEBUG")
+	cliVersion         = "0.0.2"
 )
 
 type Response struct {
@@ -20,42 +30,17 @@ type Response struct {
 	Data    interface{}
 }
 
-type paramRequest struct {
-	Application string
-	Environment string
-	Version     string
-	Landscape   string
-}
+type paramRequest map[string]string
 
 func (p paramRequest) valid() bool {
-	if p.Application == "" || p.Environment == "" || p.Version == "" || p.Landscape == "" {
-		return false
-	}
+	fmt.Println(p)
 	return true
 }
 
-func (p paramRequest) envPrefix() string {
-	return fmt.Sprintf("%s.%s.%s", p.Landscape, p.Environment, p.Application)
-}
-
-func (p paramRequest) cacheKey() string {
-	return fmt.Sprintf("%x", sha256.Sum256([]byte(p.identifier())))
-}
-
-func (p paramRequest) identifier() string {
-	return fmt.Sprintf("%s@%s", p.envPrefix(), p.Version)
-}
-
-var (
-	DebugMode = false
-	SingleKeyMode = false
-	CACHE = make(map[string]Response)
-	region = os.Getenv("AWS_REGION")
-	debug = os.Getenv("DEBUG")
-	SingleKey = os.Getenv("SINGLE_KEY_MODE")
-)
-
 func main() {
+	if env, ok := os.LookupEnv("ENVIRONMENT"); ok {
+		currentEnvironment = env
+	}
 	api()
 }
 
@@ -71,7 +56,7 @@ func api() {
 	//in debug mode no caching takes place
 	//logs are produced in greater detail
 	if debug != "" {
-		log.Printf("DEBUG flag set to %+v - attempting to parse to boolean",debug)
+		log.Printf("DEBUG flag set to %+v - attempting to parse to boolean", debug)
 		debugenabled, err := strconv.ParseBool(debug)
 		if err != nil {
 			log.Printf("Warning: Could not parse debug flag, value provided was %s\n %s", DebugMode, err.Error())
@@ -79,16 +64,8 @@ func api() {
 			DebugMode = false
 		} else {
 			DebugMode = debugenabled
-			log.Printf("debug mode set to %+v",DebugMode)
+			log.Printf("debug mode set to %+v", DebugMode)
 		}
-	}
-	if SingleKey != "" {
-		sk, err := strconv.ParseBool(SingleKey)
-		if err != nil {
-			log.Fatalf("Could not start application, unknown value '%v' set " +
-				"for SINGLE_KEY_MODE ENV VAR - true or false required",SingleKey)
-		}
-		SingleKeyMode = sk
 	}
 	log.Println("Started: Ready to serve")
 	log.Fatal(http.ListenAndServe(":8080", loggedRouter)) //todo, refactor to make port dynamic
@@ -112,47 +89,38 @@ func parseParamRequestBody(b io.ReadCloser) paramRequest {
 
 func (p paramRequest) getData() map[string]string {
 	c := ssmClient{NewClient(region)}
-	if SingleKeyMode {
-		paramName := c.WithPrefix(fmt.Sprintf("%s.%s.%s.%s", p.Landscape, p.Environment, p.Application,p.Version))
-		return paramName.IncludeHistory(c).withVersion(p.Version)
+	var params []string
 
+	for _, v := range p {
+		params = append(params, fmt.Sprintf("/%s/%s", currentEnvironment, v))
 	}
-	paramNames := c.WithPrefix(p.envPrefix()) //todo, provide the full known param, is composite key
-	return paramNames.IncludeHistory(c).withVersion(p.Version) //todo, return error
+	o, err := c.ParamList(params...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// param keys cant be used more than once this way...
+	data := map[string]string{}
+	for k, v := range p {
+		for _, par := range o.Parameters {
+			if fmt.Sprintf("/%s/%s", currentEnvironment, v) == *par.Name {
+				// base64 encode
+				data[k] = base64.StdEncoding.EncodeToString([]byte(*par.Value))
+			}
+		}
+	}
+	return data
 }
 
 func envHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("1")
 	p := parseParamRequestBody(r.Body)
 	if !p.valid() {
 		badRequest(w, p)
 		return
 	}
-	log.Printf("Processing request for %s uniquely identified as %+v", p.identifier(), p.cacheKey())
-	cached, ok := CACHE[p.cacheKey()]
-	if ok {
-		if DebugMode {
-			log.Println("Bypassing response cache due to debug mode")
-		} else {
-			log.Printf("Retrieved parameters from cache")
-			JSONResponseHandler(w, cached)
-			return
-
-		}
-	}
 	data := p.getData()
-	resp := Response{status:http.StatusOK, Data:data} //todo, check length of list before returning
-	//only cache data when elements were found
-	//possible bug - existing versions where new elements are added will still return cached data
-	//should not be a problem since container will be restarted upon config changes
-	//latest is treated as a special version indicator which should not be cached
-	if len(data) > 0 && p.Version != "latest" {
-		if DebugMode {
-			log.Println("Skipping response caching due to debug mode")
-		} else {
-			log.Println("Caching result set")
-			CACHE[p.cacheKey()] = resp
-		}
-	}
+	resp := Response{status: http.StatusOK, Data: data} //todo, check length of list before returning
 	JSONResponseHandler(w, resp)
 }
 
@@ -161,16 +129,15 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	var m = make(map[string]string)
 	m["error"] = fmt.Sprintf("Route %s not found with method %s, please check request and try again",
 		r.URL.Path, r.Method)
-	resp := Response{Data:m, status:http.StatusNotFound}
+	resp := Response{Data: m, status: http.StatusNotFound}
 	JSONResponseHandler(w, resp)
 }
 
 func badRequest(w http.ResponseWriter, p paramRequest) {
 	w.Header().Set("Content-Type", "application/json")
 	var m = make(map[string]string)
-	expected := strings.ToLower(fmt.Sprintf("%+v", paramRequest{"STRING", "STRING", "STRING", "STRING"}))
-	m["error"] = fmt.Sprintf("Bad request, expected: %s, got: %s", expected, strings.ToLower(fmt.Sprintf("%+v", p)))
-	resp := Response{Data:m, status:http.StatusBadRequest}
+	m["error"] = fmt.Sprintf("bad request: %s", p)
+	resp := Response{Data: m, status: http.StatusBadRequest}
 	JSONResponseHandler(w, resp)
 }
 
